7c335bd1095f302eca22dd3f4309d818aaeeb6f7
unknown <cneville416@wit-xdvm-042.MILLARDPS.NET>
2012-11-14 10:57:57 -0600

    delete contact button and deletes the contact

--- pages/contacts.php
+++ pages/contacts.php
@@ -19,11 +19,13 @@
 	$first3 = substr($contact_info[3],0,3);
 	$next3 = substr($contact_info[3],3,3);
 	$last4 = substr($contact_info[3],6,4);
-?>
+?>	 
 	<div class="contact">
-		<img src="http://placehold.it/100x100/DB0000/ffffff" alt="avatar" />
+		<img src="http://placehold.it/80x80/DB0000/ffffff" alt="avatar" />
 		<h3><?php echo"$name"?></h3>
 		<p class="email"><a class="btn btn-primary" href="mailto:<?php echo"$email"?>"><i class="icon-envelope icon-white"></i><?php echo"$email"?></a></p>
 		<p class="phone"><i class="icon-th"></i><?php echo "($first3) $next3-$last4"?></p>
+		<a class="btn btn-danger remove" href="./actions/delete_contact.php?email=<?php echo $email ?>">Delete</a>
 	</div>
+	
 <?php  }?>
